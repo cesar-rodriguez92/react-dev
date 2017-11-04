937ab805bee72085003160f01e4bdf7c98e51584
Cesar Alberto Rodriguez Toledo <cesar.2010.11@gmail.com>
2017-11-04 10:24:10 -0500

    Se agrega label for name

--- clase5/app01/src/Form.js
+++ clase5/app01/src/Form.js
@@ -16,7 +16,7 @@ const ContactForm = (props) =>{
   const { handleSubmit } = props;
   return(
     <form onSubmit = { handleSubmit }>
-      <Field name='name' component='input' type='text' />
+      <Field name='name' component={renderField} type='text'  label='Nombre'/>
       <Field name='email' component={renderField} type='email'label='Correo Electronico' />
       <button
         type='submit'>Enviar</button>
