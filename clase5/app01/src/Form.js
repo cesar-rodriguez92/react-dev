c95b748cf6a0bf2098ef370ece4b572ddba05c65
Cesar Alberto Rodriguez Toledo <cesar.2010.11@gmail.com>
2017-11-04 11:49:39 -0500

    Validaciones v 2

--- clase5/app01/src/Form.js
+++ clase5/app01/src/Form.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 
+const validateForm = (values) =>{
+  const errores = {};
+  //Validar campo promedio
+  if(values.promedioFinal < 0 || values.promedioFinal > 20){
+    errores.promedioFinal = 'El promedio final debe ser mayor o igual a 0 o menor o igual a 20';
+  }
+  return errores;
+}
+
 const renderField = (props) => {
-  const {type, input, label} = props;
+  const {type, input, label, meta} = props;
   console.log(props);
   return(
     <div>
       <label htmlFor={input.name}>{label}</label>
       <input type={type} {...input}/>
+      { meta.touched && meta.error && <span> {meta.error}</span>  }
     </div>
   );
 }
@@ -16,10 +26,10 @@ const ContactForm = (props) =>{
   const { handleSubmit } = props;
   return(
     <form onSubmit = { handleSubmit }>
-      <Field name='name' component={renderField} type='text'  label='Nombre'/>
-      <Field name='email' component={renderField} type='email'label='Correo Electronico' />
-      <Field name='edad' component={renderField} type='number'label='Edad' normalize = {validarEdad('edad')}  />
-      
+      <Field name = 'name'  component  = {renderField} type = 'text'   label = 'Nombre'/>
+      <Field name = 'email' component  = {renderField} type = 'text'   label = 'Correo Electronico' />
+      <Field name = 'edad'  component  = {renderField} type = 'number' label = 'Edad' normalize = {validarEdad('edad')}  />
+      <Field name = 'promedioFinal' component = {renderField} type = 'number' label = 'Promedio Final'></Field>
       <button
         type='submit'>Enviar</button>
     </form>
@@ -33,5 +43,7 @@ const validarEdad = otherField => (value, previousValue, allValues) => value > 0
 
 export default reduxForm({
   form: 'contact-form',
-  initialValues : {edad: 0}
+  initialValues : { edad: 0,
+                    promedioFinal: 0 },
+  validate : validateForm
 })(ContactForm);
